abc48901925268e9cb8368d2905a5260e8c68ee6
Ashif4354 <ashif.abhas@gmail.com>
2026-01-05 08:58:02 +0530

    refactor(profiles): rename profiles_dir to environment_dir

--- src/Engine/lib/core/Profiles.py
+++ src/Engine/lib/core/Profiles.py
@@ -11,12 +11,12 @@ from ..settings import settings
 logger: Logger = getLogger(f"streamstorm.{__name__}")
 
 class Profiles:
-    __slots__: tuple[str, ...] = ('app_data_dir', 'profiles_dir', 'base_profile_dir')
+    __slots__: tuple[str, ...] = ('app_data_dir', 'environment_dir', 'base_profile_dir')
     
     def __init__(self) -> None:
         
         self.app_data_dir: str = user_data_dir("StreamStorm", "DarkGlance")
-        self.profiles_dir: str = self.__get_environment_dir()
+        self.environment_dir: str = self.__get_environment_dir()
         self.base_profile_dir: str = self.__get_base_profile_dir()
 
 
@@ -25,7 +25,7 @@ class Profiles:
 
 
     def __get_base_profile_dir(self) -> str:
-        return join(self.profiles_dir, "BaseProfile")
+        return join(self.environment_dir, "BaseProfile")
 
 
     def get_available_temp_profiles(self, for_deletion: bool = False, total_channels: int = 0, from_mcp: bool = False) -> list[str]:
@@ -38,7 +38,7 @@ class Profiles:
             return [f"temp_profile_{i}" for i in range(1, total_channels * 2)]
         
         temp_profiles: list[str] = [
-            profile for profile in listdir(self.profiles_dir) if profile.startswith("temp_profile_")
+            profile for profile in listdir(self.environment_dir) if profile.startswith("temp_profile_")
         ]
         
         no_of_profiles: int = len(temp_profiles)
@@ -53,14 +53,14 @@ class Profiles:
     def get_profile_dir(self, index: int, profiles: list[str]) -> str:
 
         index %= len(profiles)
-        tempdir: str = join(self.profiles_dir, profiles[index])
+        tempdir: str = join(self.environment_dir, profiles[index])
 
         return tempdir
     
-    def __delete_profiles_dir(self) -> None:
-        if exists(self.profiles_dir):
-            rmtree(self.profiles_dir, ignore_errors=True)
-            logger.info(f"Profiles directory {self.profiles_dir} deleted.")
+    def __delete_environment_dir(self) -> None:
+        if exists(self.environment_dir):
+            rmtree(self.environment_dir, ignore_errors=True)
+            logger.info(f"Environment directory {self.environment_dir} deleted.")
 
     def __create_base_profile(self) -> None:
         if exists(self.base_profile_dir):
@@ -76,7 +76,7 @@ class Profiles:
 
         logger.info(f"Creating {profile}")
 
-        tempdir: str = join(self.profiles_dir, profile)
+        tempdir: str = join(self.environment_dir, profile)
 
         makedirs(tempdir, exist_ok=True)
         
@@ -94,7 +94,7 @@ class Profiles:
         logger.info(f"{profile} created")
 
     def create_profiles(self, count: int) -> None:
-        self.__delete_profiles_dir()
+        self.__delete_environment_dir()
         self.__create_base_profile()
 
         if settings.login_method == "cookies":
@@ -105,9 +105,8 @@ class Profiles:
         with ThreadPoolExecutor() as executor:
             executor.map(self.__create_profile, profiles)
                 
-            
     def delete_all_temp_profiles(self) -> None:
         
-        self.__delete_profiles_dir()
+        self.__delete_environment_dir()
 
 __all__: list[str] = ["Profiles"]
